2812fac21a15c9c492d59d4a706b01532a98227b
carvalho28 <carvalhoo287@gmail.com>
2022-11-13 00:17:10 +0000

    Base design for new record
    Signed-off-by: carvalho28 <carvalhoo287@gmail.com>

--- lib/views/record.dart
+++ lib/views/record.dart
@@ -1,3 +1,4 @@
+import 'package:explode/constants/general.dart';
 import 'package:explode/main.dart';
 import 'package:explode/models/record.dart';
 import 'package:explode/providers/answers_provider.dart';
@@ -79,27 +80,131 @@ class _RecordState extends State<Record> {
 
   @override
   Widget build(BuildContext context) {
-    //  print all records from the database
-    // get all records from the database
-
     printRecords();
 
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
-        title: const Text('Record'),
+        backgroundColor: primaryColor,
+        elevation: 0,
       ),
       body: FutureBuilder(
         future: refreshRecord(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return Column(
-              children: [
-                Text('Your score: $_correctAnswers'),
-                if (_record == null)
-                  const Text('New record!')
-                else
-                  Text('Old record: ${_record!.correctAnswers}'),
-              ],
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: 1),
+                    duration: const Duration(seconds: 3),
+                    builder: (context, double value, child) {
+                      return Transform.scale(
+                        scale: value,
+                        child: child,
+                      );
+                    },
+                    child: const Text(
+                      'Game\nOver',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 60,
+                        fontFamily: fontGameOver,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  // if new record is set, display it and say how many points were scored
+                  if (_record == null ||
+                      _correctAnswers > _record!.correctAnswers)
+                    const Text(
+                      'New record!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                      ),
+                    ),
+                  // if new record is not set, display the old record and say how many points were scored
+                  if (_record != null &&
+                      _correctAnswers <= _record!.correctAnswers)
+                    Text(
+                      'Record: ${_record!.correctAnswers}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    'Score: $_correctAnswers!',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                  // Text(
+                  //   'Your score: $_correctAnswers',
+                  //   style: const TextStyle(
+                  //     color: Colors.white,
+                  //     fontSize: 30,
+                  //   ),
+                  // ),
+                  // const SizedBox(
+                  //   height: 20,
+                  // ),
+                  // Text(
+                  //   'Record: ${_record?.correctAnswers ?? 0}',
+                  //   style: const TextStyle(
+                  //     color: Colors.white,
+                  //     fontSize: 30,
+                  //   ),
+                  // ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: secondaryColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 20,
+                      ),
+                      // add shadow to the button
+                      elevation: 15,
+                    ),
+                    onPressed: () {
+                      saveRecord();
+                      Provider.of<EndTimer>(context, listen: false)
+                          .resetEndTimer();
+                      Provider.of<Answers>(context, listen: false)
+                          .resetCorrect();
+                      Provider.of<Answers>(context, listen: false).resetWrong();
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/main-menu',
+                        (route) => false,
+                      );
+                    },
+                    child: const Text(
+                      'Back to Main Menu',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           } else {
             return const Center(
@@ -108,22 +213,48 @@ class _RecordState extends State<Record> {
           }
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          saveRecord();
-          Provider.of<EndTimer>(context, listen: false).resetEndTimer();
-          Provider.of<Answers>(context, listen: false).resetCorrect();
-          Provider.of<Answers>(context, listen: false).resetWrong();
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const MainMenu(),
-            ),
-            (route) => false,
-          );
-        },
-        child: const Icon(Icons.check),
-      ),
     );
   }
 }
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Record'),
+    //   ),
+    //   body: FutureBuilder(
+    //     future: refreshRecord(),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    //         return Column(
+    //           children: [
+    //             Text('Your score: $_correctAnswers'),
+    //             if (_record == null)
+    //               const Text('New record!')
+    //             else
+    //               Text('Old record: ${_record!.correctAnswers}'),
+    //           ],
+    //         );
+    //       } else {
+    //         return const Center(
+    //           child: CircularProgressIndicator(),
+    //         );
+    //       }
+    //     },
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: () {
+    //       saveRecord();
+    //       Provider.of<EndTimer>(context, listen: false).resetEndTimer();
+    //       Provider.of<Answers>(context, listen: false).resetCorrect();
+    //       Provider.of<Answers>(context, listen: false).resetWrong();
+    //       Navigator.pushAndRemoveUntil(
+    //         context,
+    //         MaterialPageRoute(
+    //           builder: (context) => const MainMenu(),
+    //         ),
+    //         (route) => false,
+    //       );
+    //     },
+    //     child: const Icon(Icons.check),
+    //   ),
+    // );
